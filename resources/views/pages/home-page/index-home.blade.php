47be047203857afc69bd47d46f0c0047e3a27ced
boris2442 <aubinborissimotsebo@gmail.com>
2025-06-11 16:04:33 +0200

    configuration du projet

--- resources/views/pages/home-page/index-home.blade.php
+++ resources/views/pages/home-page/index-home.blade.php
@@ -1,78 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Responsive Portfolio Website</title>
-    <!-- Fonts -->
-    <link rel="stylesheet" href="{{ asset('template/assets/fonts/unicons/css/line.css') }}">
-
-    <!-- CSS -->
-    <!-- SWIPER CSS -->
-    <link rel="stylesheet" href="{{ asset('template/assets/css/swiper-bundle.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('template/assets/css/styles.css') }}">
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
-    <!-- Styles / Scripts -->
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    
-      
-    @endif
-</head>
-
-<body>
-    <!--==================== HEADER ====================-->
-    <header class="header" id="header">
-        <nav class="nav container">
-            <a href="#home" class="nav_logo">
-                John Doe
-            </a>
-
-            <div class="nav_menu" id="nav-menu">
-                <ul class="nav_list">
-                    <li class="nav-item">
-                        <a href="#home" class="nav_link active_link">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#about" class="nav_link">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#skills" class="nav_link">Skills</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#services" class="nav_link">Services</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#portfolio" class="nav_link">Portfolio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#contact" class="nav_link">Contact Me</a>
-                    </li>
-                </ul>
-
-                <div class="nav_close" id="nav-close">
-                    <i class="uil uil-times nav_close" id="nav-close"></i>
-                </div>
-            </div>
-
-            <div class="nav_btns">
-                <!--===== THEME CHANGE BUTTON =====-->
-                <i class="uil uil-moon change_theme" id="theme-button"></i>
-
-                <div class="nav_toogle" id="nav-toggle">
-                    <i class="uil uil-bars"></i>
-                </div>
-            </div>
-        </nav>
-    </header>
-
-    <!--==================== MAIN ====================-->
+@extends('layouts.website.layout-website')
+@section('content')
+ <!--==================== MAIN ====================-->
     <main class="main">
         <!--==================== HOME ====================-->
         <section class="home section" id="home">
@@ -874,20 +802,4 @@
             </div>
         </section>
     </main>
-
-   
-
-    <!--==================== SCROLL TOP ====================-->
-    <a href="#" class="scrollup" id="scroll-up">
-        <i class="uil uil-arrow-up scrollup_icon"></i>
-    </a>
-
-    <!--==================== SWIPER JS ====================-->
-    <script src="{{ asset('template/assets/js/swiper-bundle.min.js') }}"></script>
-
-    <!--==================== MAIN JS ====================-->
-    <script src="{{ asset('template/assets/js/main.js') }}"></script>
-</body>
-
-
-</html>
+@endsection
