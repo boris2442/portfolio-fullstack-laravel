ec3506706fe9a304de291c38dc648cb3f9441372
boris2442 <aubinborissimotsebo@gmail.com>
2025-06-11 17:00:00 +0200

    text

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 Route::get('/', [HomeController::class, 'index'])->name('home-page');
+Route::get('/dashboard', [DashboardController::class, 'dashboard-index'])->name('home-dashboard-page');
